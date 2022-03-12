6896ec5ac2a9729489c805ca95783edb9eef51c0
michaworku <mikias27worku@gmail.com>
2022-03-12 13:24:35 +0300

    nav bar added and form custom hook created

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,15 @@ import * as smoothscroll from 'smoothscroll-polyfill'
 import { Navbar } from './components'
 import { Auth, CreatorOrTag, Favorites, Home, Overview, PostDetails } from './pages'
 
+import { CssBaseline } from "@material-ui/core";
+
 smoothscroll.polyfill()
 
 const App: FC = () => {
   return (
     <Router>
+      <CssBaseline />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Overview />} />
         <Route path="/auth" element={<Auth />} />
