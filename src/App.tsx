c84f515e0bbfaf7ed4c47727ff4d193c4cf32ad2
michaworku <mikias27worku@gmail.com>
2022-03-12 12:28:43 +0300

    file and forlder structure - pages and components are created - pages are routed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { FC } from 'react'
 
-const App = () => {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+// @ts-ignore
+import * as smoothscroll from 'smoothscroll-polyfill'
+
+import { Navbar } from './components'
+import { Auth, CreatorOrTag, Favorites, Home, Overview, PostDetails } from './pages'
+
+smoothscroll.polyfill()
+
+const App: FC = () => {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Overview />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/posts" element={<Home />} />
+        <Route path="/posts/search" element={<Home />} />
+        <Route path="/posts/:id" element={<PostDetails />} />
+        {/* <Route path={["/creators/:name", "/tags/:name"]} element={<CreatorOrTag />} /> */}
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export { default as Auth } from './Auth/Auth'
+export { default as Favorites } from './Favorites/Favorites'
+export { default as Home } from './Home/Home'
+export { default as Overview } from './Overview/Overview'
+export { default as PostDetails } from './PostDetails/PostDetails'
+export { default as CreatorOrTag } from './CreatorOrTag/CreatorOrTag'
